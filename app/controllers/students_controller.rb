6d02b9b6e55ec055d27236ce507aa42b5e3c10ab
carreuky <carreuky@gmail.com>
2022-09-24 00:07:46 +0300

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,11 @@
 class StudentsController < ApplicationController
 
-  def index
+  def in
     students = Student.all
+    render json: students
+  end
+  def index
+    students = Student.find(params[:id])
     render json: students
   end
 
